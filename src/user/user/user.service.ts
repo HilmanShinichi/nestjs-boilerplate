557242174e880c80db0095162df49d496b7e5b8a
Hilman Septiana <hilmanseptiana19@gmail.com>
2024-11-11 22:54:49 +0700

    Validation

--- src/user/user/user.service.ts
+++ src/user/user/user.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
-
+import { ValidationService } from 'src/validation/validation/validation.service';
+import { z } from 'zod';
 @Injectable()
 export class UserService {
+    constructor(private validationService: ValidationService) {}
     sayHello(firstName: string, lastName: string): string {
-        return `Hello ${firstName || 'Guest'} ${lastName || ''}`;
+        const schema = z.string().min(3).max(100);
+     // Validasi `firstName` dan `lastName` secara terpisah
+     const validatedFirstName = this.validationService.validate(schema, firstName);
+     const validatedLastName = this.validationService.validate(schema, lastName);
+
+     return `Hello ${validatedFirstName || 'Guest'} ${validatedLastName || ''}`;
     }
 }
